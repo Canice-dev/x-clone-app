d936a8b0bf146755600f2edae845d6aa0e1c4666
Canicedev <canice.aba.dev@gmail.com>
2026-07-12 23:02:17 +0100

    notifications and arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,8 @@ import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 // connectDB();
 
@@ -23,6 +26,7 @@ app.get("/", (req, res) => res.send("Hello from server"));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
