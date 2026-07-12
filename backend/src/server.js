be3ef12b41681238e493dbdaf55b7d9e5ed0f543
Canicedev <canice.aba.dev@gmail.com>
2026-07-12 21:29:57 +0100

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,23 @@ import express from "express";
 import { ENV } from "./config/env.js";
 // import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+import userRoutes from "./models/user.model.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
 
 // connectDB();
 
 app.get("/", (req, res) => res.send("Hello from server"));
+
+app.use("/api/user", userRoutes);
 
 // app.listen(ENV.PORT, () =>
 //   console.log("Server is up and running on PORT:", ENV.PORT),
