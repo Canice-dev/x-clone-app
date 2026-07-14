e5700fa5f9d6f81ab30df799e0b992534253c748
Canicedev <canice.aba.dev@gmail.com>
2026-07-14 21:04:22 +0100

    feat: (oauth signin google and apple)

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,10 +3,10 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: "./global.css" });
 
-config.transformer.transformIgnorePatterns = [
-  "node_modules/(?!(react-native|@react-native|expo|@expo|some-pkg)/)",
-];
+// config.transformer.transformIgnorePatterns = [
+//   "node_modules/(?!(react-native|@react-native|expo|@expo|some-pkg)/)",
+// ];
 
-module.exports = config;
+// module.exports = config;
